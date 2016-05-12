7e7c00d62c6ca994fd7dee5a3d0d2c63d9bd5e59
fowlks <fowlks@msu.edu>
2016-05-12 17:10:12 -0400

    Added additional TeatNG cases H2 Database is now working

--- Todo-App-Web/src/test/java/com/gmail/fowlk1kd/test/service/TodoTaskServiceTest.java
+++ Todo-App-Web/src/test/java/com/gmail/fowlk1kd/test/service/TodoTaskServiceTest.java
@@ -1,49 +1,51 @@
 package com.gmail.fowlk1kd.test.service;
 
-import org.testng.annotations.Test;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.Assert;
 import org.testng.annotations.Test;
-import org.testng.Assert.*;
+
 import com.gmail.fowlk1kd.domain.TodoTask;
 import com.gmail.fowlk1kd.services.TodoService;
 
 @ContextConfiguration(locations = {"classpath:applicationContext.xml"})
 public class TodoTaskServiceTest extends AbstractTestNGSpringContextTests {
 
-
+    private int MAX_TASKS = 10000;
+  
     @Autowired
 	private transient TodoService todoService;
 	
-	@Test
-	public void findOpenTasks() throws Exception {
-		
-		TodoTask task = new TodoTask();
-		task.setDescription("Test Case");
-		
-		
-		todoService.addTask(task);
-		
-		//List<TodoTask> tasks = todoService.addTask( );
-		
-		//task.setDescription("Unit Test - Sample");
-		
-        //Assert.assertEquals( "Oh man, names don't match!", "Joe Smith", bean.getSessionTimeout );
+	@Test(dependsOnMethods = "addNewTasks")
+	public void findOpenTasks() {		
+		List<TodoTask> tasks =  todoService.getAllOpenTasks();		
+		Assert.assertFalse( tasks.isEmpty() );		
     }
 	
+	@Test(dependsOnMethods = "addNewTasks")
+    public void findAllTasks() {        
+        List<TodoTask> tasks =  todoService.getAllTasks();        
+        Assert.assertFalse( tasks.isEmpty() );      
+    }	
 	
 	@Test
-    public void doNothing()  {
+    public void addNewTasks() {	  
+	  for ( int i = 0; i < MAX_TASKS; i++ ) 
+	  {
+	    TodoTask task = new TodoTask();
+	    
+        task.setDescription("Unit Test - Sample #".concat( String.valueOf(i) ));        
+        todoService.addTask(task);        
         
-        if( todoService == null )
-          org.testng.Assert.fail("Service is not null");
-          
-        //List<TodoTask> tasks = todoService.addTask( );
-        
-        //task.setDescription("Unit Test - Sample");
-        
-        //Assert.assertEquals( "Oh man, names don't match!", "Joe Smith", bean.getSessionTimeout );
+        task = null;
+	  }
+      
+	  List<TodoTask> tasks =  todoService.getAllOpenTasks();
+	  
+	  Assert.assertEquals( tasks.size(), MAX_TASKS, "Incorrect # of tasks" );
     }
 	
 }
